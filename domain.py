019c775ef358f436389ba11f12d84b180fa67d22
Jonathan Arp <jarp@nd.edu>
2021-11-02 16:01:22 -0400

    comments in code

--- domain.py
+++ domain.py
@@ -1,23 +1,33 @@
+# random is used in ChanceEnging
 import random
 
-class Player:
 
+class Player:
+    # __init__() gets executed when Player() is called
+    # not necessary to include but very useful in most cases
     def __init__(self, first_name, age):
+        # self is a hook into the object created. It's how you add attributes
         self.first_name = first_name
+        # the `_` signifies that the attribute should be treated as private
         self._age = age
 
+    # these are functions that are hooked into the object but their strucure is a plain old function
     def age(self):
         return self._age
 
     def full_name(self):
         return self.first_name
 
-class Game:
 
+class Game:
+    # player is an argument you can use with Game() >> Game(players=...)
+    # the input is stored in an instance variable (called an attribute)
     def __init__(self, players = []):
         self._players = players
 
+    # a function that needs to be completed
     def next_player(self):
+        # `pass` allows you to have an empty execution block
         pass
 
     def current_player(self):
@@ -34,11 +44,14 @@ class ChanceEngine:
         selected_index = random.randint(0, num_of_values - 1)
         return self._values[selected_index]
 
+# this is the syntax for using inheritance
+# Dice will inherit the attributes and methods of ChanceEnging
 class Dice(ChanceEngine):
 
     def roll(self):
         print("roll()")
         return self.random_value()
+
 
 class Coin(ChanceEngine):
 
@@ -46,7 +59,9 @@ class Coin(ChanceEngine):
         self._values = ['Heads', 'Tails']
 
     def flip(self):
+        # when you call a method on the class instance, you use `self` just like attributes
         return self.random_value()
+
 
 class StandardDice(Dice):
 
--- test.py
+++ test.py
@@ -1,11 +1,14 @@
+# import everything from the file domain.py
 from domain import *
 import os
 
 os.system("clear")
 
+# this is how you create objects from a class file
 player = Player("jonathan", 23)
 player2 = Player("sebastian", 4)
 
+# create a game and pass in a list of players
 game = Game([player, player2])
 
 print(f"{player.first_name} is {player.age()} years old")
@@ -15,4 +18,5 @@ dice = StandardDice()
 coin = Coin()
 
 for i in range(10):
+    # loop 10 times, and print the result of the dice.roll() method
     print(dice.roll())

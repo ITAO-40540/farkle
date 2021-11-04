3c6a5ae92c879333a576ae5dd357bb5a1e44d9a2
Jonathan Arp <jarp@nd.edu>
2021-11-04 10:33:51 -0400

    starting a python script to implement game

--- play.py
+++ play.py
@@ -6,3 +6,13 @@ print("#################################################")
 print("############## Play Farkle ######################")
 print("#################################################")
 print("\n\n")
+
+number_of_players = int(input("How many people playing today? "))
+players = []
+for i in range(number_of_players):
+    player_name = input("What is player{i + 1}'s name? ")
+    players.append(Player(player_name))
+
+game = Game(players)
+
+print(game)

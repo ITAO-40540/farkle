5b45da3875869685129dfdd39f5db8a63d8744a8
Jonathan Arp <jarp@nd.edu>
2021-11-04 10:19:09 -0400

    added comments to test script

--- test_scoring.py
+++ test_scoring.py
@@ -3,6 +3,8 @@ from domain import *
 import os
 os.system("clear")
 
+# create a dictionary that encapsulates the rules for aquiring points
+# key is the expected points, values are a list of dice that would get those points
 scenarios = {
     500: [5, 5, 5],
     1000: [1, 1, 1],
@@ -10,14 +12,22 @@ scenarios = {
     400: [4, 4, 4],
     200: [1, 5, 5]
 }
+# gonna create a bunch of scoring sets from dictionary above
 scoring_sets = []
 
+# loop though all the keys
 for k in scenarios.keys():
+
     dice = []
     for i in scenarios[k]:
+        # instantiate the dice objects with the values from the dictionary
         dice.append(StandardDie(i))
+
+    # create a scoring set from dice and append to the list
     scoring_sets.append(ScoringSet(dice))
 
+# once done, loop though all the scoring sets (or the dictionary keys)
+# print out the two values and they should match
 for i in range(len(scoring_sets)):
     print(scoring_sets[i].points(), ' == ', list(scenarios.keys())[i])
 

2be3086ca4850cb7b78b4b7053637265f725294a
Jonathan Arp <jarp@nd.edu>
2021-11-04 09:12:34 -0400

    added code for scoring set from previous version of program

--- domain.py
+++ domain.py
@@ -44,6 +44,7 @@ class ChanceEngine:
         selected_index = random.randint(0, num_of_values - 1)
         return self._values[selected_index]
 
+
 # this is the syntax for using inheritance
 # Dice will inherit the attributes and methods of ChanceEnging
 class Dice(ChanceEngine):
@@ -67,3 +68,98 @@ class StandardDice(Dice):
 
     def __init__(self):
         self._values = range(1,7)
+
+
+class ScoringSet:
+    def __init__(self, dice):
+        self._dice = dice
+        self._dice_as_integers = []
+
+    def points(self):
+        self.dice_to_integers()
+        value_counts = self.value_counts(self._dice_as_integers)
+
+        if self._dice_as_integers == [1, 2, 3, 4, 5, 6]:
+            return 3000
+        elif len(value_counts.keys()) == 3 and list(value_counts.values()) == [2, 2, 2]:
+            return 1500
+        else:
+            return self.extract_points(value_counts)
+
+    def len(self):
+        return len(self._dice)
+
+    def extract_points(self, value_counts):
+        point_total = 0
+
+        try:
+            index_of_threes = list(value_counts.values()).index(3)
+            value_with_threes = list(value_counts.keys())[index_of_threes]
+            value_counts.pop(value_with_threes)
+            point_total += self.points_from_threes(value_with_threes)
+        except:
+            index_of_threes = None
+        try:
+            index_of_fours = list(value_counts.values()).index(4)
+            value_with_fours = list(value_counts.keys())[index_of_fours]
+            value_counts.pop(value_with_fours)
+            point_total += self.points_from_fours(value_with_fours)
+        except:
+            # raise
+            index_of_fours = None
+
+        point_total += self.points_from_singles(value_counts)
+
+        return point_total
+
+    def points_from_threes(self, value):
+        if value == 1:
+            return 1000
+        else:
+            return value * 100
+
+    def points_from_fours(self, value):
+        total_points = self.points_from_threes(value)
+        total_points += self.points_from_singles({value: 1})
+        return total_points
+
+    def points_from_singles(self, value_counts):
+        point_total = 0
+        for die_val in value_counts.keys():
+            die_count = value_counts[die_val]
+            for v in range(die_count):
+                if die_val == 1:
+                    point_total += 100
+                elif die_val == 5:
+                    point_total += 50
+
+        return point_total
+
+    def value_counts(self, dice_values):
+        value_counts = {}
+        for i in dice_values:
+            if i in value_counts:
+                value_counts[i] += 1
+            else:
+                value_counts[i] = 1
+        return value_counts
+
+    def extract_points_from_threes(self, dice_values):
+        if dice_values == [1, 1, 1]:
+            return 1000
+        elif dice_values == [5, 5, 5]:
+            return 500
+        elif dice_values == [4, 4, 4]:
+            return 400
+        elif dice_values == [3, 3, 3]:
+            return 300
+        elif dice_values == [2, 2, 2]:
+            return 200
+        else:
+            return 0
+
+    def dice_to_integers(self):
+        self._dice_as_integers = []
+        for die in self._dice:
+            self._dice_as_integers.append(int(die.value()))
+        self._dice_as_integers.sort()

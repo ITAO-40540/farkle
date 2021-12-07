7c0d7d73860c9010cb30df9db720320a19b0d3b6
Jonathan Arp <jarp@nd.edu>
2021-12-07 09:11:38 -0500

    adding a test for validating rolls

--- models/roll.py
+++ models/roll.py
@@ -2,6 +2,7 @@ class Roll:
 
     def __init__(self, dice):
         self._dice = dice
+        self.validation_errors = []
 
     def roll(self):
         for die in self._dice:
@@ -12,4 +13,12 @@ class Roll:
         for die in self._dice:
             str = str + f"{die.value()}"
         return str
+
+    def is_valid(self):
+        for die in self._dice:
+            if type(die) != 'StandardDie':
+                self.validation_errors << 'One or more die are not standard'
+                return False
+            self.validation_errors = []
+            return True
 

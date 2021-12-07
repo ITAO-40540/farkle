6c16bc01721ed8ad0cc60319f472a4d6f3785643
Jonathan Arp <jarp@nd.edu>
2021-12-07 09:00:13 -0500

    added test for more than one first name

--- tests/models/player_test.py
+++ tests/models/player_test.py
@@ -7,3 +7,7 @@ class TestGame:
     def test_player_name_is_capitalized(self):
         me = Player('jonathan')
         assert me.first_name == 'Jonathan'
+
+    def test_player_with_multiple_names_is_capitalized(self):
+        me = Player('anna beth')
+        assert me.first_name == 'Anna Beth'

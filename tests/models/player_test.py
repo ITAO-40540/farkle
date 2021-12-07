c4af9ecbc1550fcaf9833aff990af0ef0150aa4d
Jonathan Arp <jarp@nd.edu>
2021-12-07 09:03:30 -0500

    renamed class file... oops

--- tests/models/player_test.py
+++ tests/models/player_test.py
@@ -2,12 +2,13 @@ import pytest
 from models.player import Player
 
 
-class TestGame:
+class TestPlayer:
 
     def test_player_name_is_capitalized(self):
         me = Player('jonathan')
         assert me.first_name == 'Jonathan'
 
+    # need to make sure this happens with important people with many names
     def test_player_with_multiple_names_is_capitalized(self):
         me = Player('anna beth')
         assert me.first_name == 'Anna Beth'
